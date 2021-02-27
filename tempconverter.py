306745697ab960c12dece88e16085923b16fc211
Sean Bachiller <59718043+prodseanb@users.noreply.github.com>
2021-02-26 23:05:10 -0500

    Update tempconverter.py

--- tempconverter.py
+++ tempconverter.py
@@ -1,28 +1,32 @@
-# Fahrenheit to Celsius = (F - 32) * .5556 = C
-import sys
+''' Fahrenheit to Celsius = (F - 32) * .5556 
+ Celsius to Fahrenheit = (C * 1.8) + 32
+ TEMPERATURE CONVERTER
+ @author Sean Bachiller
+ '''
+import sys 
 
-def try_again():
+def try_again(): # prompt after displaying the result
     again = input('Try again? Y or N: ')
     if again.upper() == 'Y':
         main()
     elif again.upper() == 'N':
         sys.exit()
     
-def cel_to_fahr():
+def cel_to_fahr(): # Celsius to Fahrenheit
     val = float(input('Enter the Celsius value: '))
     result = (val * 1.8) + 32
     print(str(round(val)), "degrees Celsius = ", str(round(result)), "degrees Fahrenheit")
     try_again()
     
     
-def fahr_to_cel():
+def fahr_to_cel(): # Fahrenheit to Celsius
     val = float(input('Enter the Fahrenheit value: '))
     result = (val - 32) * .5556
     print(str(round(val)), "degrees Fahrenheit = ", str(round(result)), "degrees Celsius")
     try_again()
 
         
-def main():
+def main(): # Intro 
     while True:
         try:
             print('\nTEMPERATURE CONVERTER (Fahrenheit to Celsius / Celsius to Fahrenheit)')
